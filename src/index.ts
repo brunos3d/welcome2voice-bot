b2271d91d875c00bd59fdf5f0f12735fc145ea77
Bruno Silva <bruno3dcontato@gmail.com>
2022-02-09 12:46:19 -0300

    chore: add logs to help on debug

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,15 @@ const WELCOME_SOUNDS = ['W8ab00LC-JQ'];
 const DISCORD_CLIENT = new Client(config.client);
 
 DISCORD_CLIENT.on('ready', async () => {
-  console.log(`Logged in as ${DISCORD_CLIENT.user?.tag}!`);
+  console.log('[bot logged in]', DISCORD_CLIENT.user?.tag);
+});
+
+DISCORD_CLIENT.on('guildCreate', async (guild) => {
+  console.log('[bot added to guild]', guild.name);
+});
+
+DISCORD_CLIENT.on('guildDelete', async (guild) => {
+  console.log('[bot kicked from guild]', guild.name);
 });
 
 DISCORD_CLIENT.on('voiceStateUpdate', async (oldState, newState) => {
@@ -21,18 +29,23 @@ DISCORD_CLIENT.on('voiceStateUpdate', async (oldState, newState) => {
 
   // left
   if (newState.channelID === null) {
-    console.log('user left channel', oldState.channelID);
+    console.log('[user left channel]', `(${oldState.guild.id} / ${oldState.guild.name})`, `${oldState.channelID} (${oldState.channel?.name})`);
     // if (newState.channel) playWelcomeMessage(newState.channel);
   }
   // joined
   else if (oldState.channelID === null) {
-    console.log('user joined channel', newState.channelID);
+    console.log('[user joined channel]', `(${oldState.guild.id} / ${oldState.guild.name})`, `${newState.channelID} (${newState.channel?.name})`);
 
     if (newState.channel) playWelcomeMessage(newState.channel);
   }
   // moved
   else {
-    console.log('user moved channels', oldState.channelID, newState.channelID);
+    console.log(
+      '[user moved from channel]',
+      `(${oldState.guild.id} / ${oldState.guild.name})`,
+      `${oldState.channelID} (${oldState.channel?.name})`,
+      `${newState.channelID} (${newState.channel?.name})`
+    );
     if (newState.channel) playWelcomeMessage(newState.channel);
   }
 });
